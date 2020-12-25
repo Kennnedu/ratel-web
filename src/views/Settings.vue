7daf358bb4ddc851bd7a01c0c3248c3b36ef4c58
Kennnedu <r.kenda94@gmail.com>
2020-12-26 01:10:32 +0300

    added overlay

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,75 +1,77 @@
 <template>
   <section>
     <b-container>
-      <div class="d-flex justify-content-around flex-wrap">
-        <b-card class="settings-item" no-body >
-          <b-card-header>
-            <h3>User Info</h3>
-          </b-card-header>
-          <b-card-body>
-            <b-form @submit="submitForm">
-              <b-form-group
-                id="input-group-username"
-                label="Username"
-                label-for="input-username">
-                <b-form-input
-                  id="input-username"
-                  v-model="userInfoForm.username"
-                  placeholder="Enter username"
-                  autocomplete="off"></b-form-input>
-              </b-form-group>
+      <b-overlay :show="isFetching" opacity="1" rounded="sm">
+        <div class="d-flex justify-content-around flex-wrap">
+            <b-card class="settings-item" no-body >
+              <b-card-header>
+                <h3>User Info</h3>
+              </b-card-header>
+              <b-card-body>
+                <b-form @submit="submitForm">
+                  <b-form-group
+                    id="input-group-username"
+                    label="Username"
+                    label-for="input-username">
+                    <b-form-input
+                      id="input-username"
+                      v-model="userInfoForm.username"
+                      placeholder="Enter username"
+                      autocomplete="off"></b-form-input>
+                  </b-form-group>
 
-              <b-form-group id="input-group-password" label="Password" label-for="input-password">
-                <b-form-input
-                  id="input-password"
-                  type="password"
-                  v-model="userInfoForm.password"
-                  placeholder="Enter password"
-                ></b-form-input>
-              </b-form-group>
+                  <b-form-group id="input-group-password" label="Password" label-for="input-password">
+                    <b-form-input
+                      id="input-password"
+                      type="password"
+                      v-model="userInfoForm.password"
+                      placeholder="Enter password"
+                    ></b-form-input>
+                  </b-form-group>
 
-              <b-form-group id="input-group-password-confirmation" label="Password Confirmation" label-for="input-password-confirmation">
-                <b-form-input
-                  id="input-password-confirmation"
-                  type="password"
-                  v-model="userInfoForm.passwordConfirmation"
-                  placeholder="Enter password confirmation"
-                ></b-form-input>
-              </b-form-group>
+                  <b-form-group id="input-group-password-confirmation" label="Password Confirmation" label-for="input-password-confirmation">
+                    <b-form-input
+                      id="input-password-confirmation"
+                      type="password"
+                      v-model="userInfoForm.passwordConfirmation"
+                      placeholder="Enter password confirmation"
+                    ></b-form-input>
+                  </b-form-group>
 
-              <b-button type="submit" variant="primary">Update</b-button>
-            </b-form>
-          </b-card-body>
-        </b-card>
+                  <b-button type="submit" variant="primary">Update</b-button>
+                </b-form>
+              </b-card-body>
+            </b-card>
 
-        <b-card class="settings-item" no-body v-if="gmailConnectionForm">
-          <b-card-header>
-            <h3>Gmail Connection</h3>
-          </b-card-header>
-          <b-card-body>
-            <fieldset disabled>
-              <b-form-group id="input-group-connected">
-                <b-form-checkbox
-                  id="input-connected"
-                  :checked="gmailConnectionForm.connected"
-                > Connected</b-form-checkbox>
-              </b-form-group>
-              <b-form-group id="input-group-report-sender" label="Report Sender" label-for="input-report-sender">
-                <b-form-input
-                  id="input-report-sender"
-                  :value="gmailConnectionForm.reportSender"
-                ></b-form-input>
-              </b-form-group>
-              <b-form-group id="input-group-last-fetching" label="Last Connection" label-for="input-last-fetching">
-                <b-form-input
-                  id="input-last-fetching"
-                  :value="gmailConnectionForm.lastConnection"
-                ></b-form-input>
-              </b-form-group>
-            </fieldset>
-          </b-card-body>
-        </b-card>
-      </div>
+            <b-card class="settings-item" no-body v-if="gmailConnectionForm">
+              <b-card-header>
+                <h3>Gmail Connection</h3>
+              </b-card-header>
+              <b-card-body>
+                <fieldset disabled>
+                  <b-form-group id="input-group-connected">
+                    <b-form-checkbox
+                      id="input-connected"
+                      :checked="gmailConnectionForm.connected"
+                    > Connected</b-form-checkbox>
+                  </b-form-group>
+                  <b-form-group id="input-group-report-sender" label="Report Sender" label-for="input-report-sender">
+                    <b-form-input
+                      id="input-report-sender"
+                      :value="gmailConnectionForm.reportSender"
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-form-group id="input-group-last-fetching" label="Last Connection" label-for="input-last-fetching">
+                    <b-form-input
+                      id="input-last-fetching"
+                      :value="gmailConnectionForm.lastConnection"
+                    ></b-form-input>
+                  </b-form-group>
+                </fieldset>
+              </b-card-body>
+            </b-card>
+        </div>
+        </b-overlay>
     </b-container>
   </section>
 </template>
@@ -80,6 +82,7 @@
   export default {
     data: function() {
       return {
+        isFetching: true,
         userInfoForm: {
           username: '',
           password: '',
@@ -119,7 +122,10 @@
       },
 
       fetchSettings() {
-        axios.get('/user').then(({ data }) => this.assignResponse(data))
+        axios.get('/user').then(({ data }) => { 
+          this.assignResponse(data);
+          this.isFetching = false;
+        })
       },
 
       submitForm(e){
